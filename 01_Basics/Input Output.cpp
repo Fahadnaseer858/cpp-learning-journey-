8ccb7c4eacbdf23a366f68608f93a72ac0bd6443
Fahad Naseer <fahadnaseer858@gmail.com>
2026-05-31 11:30:19 +0500

    Added InputOutput program

---
+++ 01_Basics/Input Output.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	string name;
+	cout<<"Enter Name: ";
+	cin>>name;
+	cout<<"Enter your Age: ";
+	cin>>age;
+	cout<<"Hello i'm "<<name<<", i'm "<<age<<" year old.";
+	
+	return 0;
+}
