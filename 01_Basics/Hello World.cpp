9d13fe77a9e08b628db2c37c2f2ef34e6c3bbaba
Fahad Naseer <fahadnaseer858@gmail.com>
2026-05-31 11:15:20 +0500

    Added Hello World Program

---
+++ 01_Basics/Hello World.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello, I'm Fahad \n";
+	cout<<"My Goal is to become an expert level Software Engineer in future Inshallah \n";
+	cout<<"I live in Punjab";
+	return 0;
+}
