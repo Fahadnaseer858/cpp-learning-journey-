b4eadf9b346c6d6b63989071d1c003c513cade92
Fahad Naseer <fahadnaseer858@gmail.com>
2026-05-31 11:19:18 +0500

    Added variables practice program

---
+++ 01_Basics/Variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	age=19;
+	string name;
+	name="Fahad";
+	cout<<"My Age is "<<age<<endl;
+	cout<<"My Name is "<<name;
+	return 0;
+}
