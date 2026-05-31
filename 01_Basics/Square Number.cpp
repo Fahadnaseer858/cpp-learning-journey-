aec7338a06b9f4114ae11341ac4c718193caf645
Fahad Naseer <fahadnaseer858@gmail.com>
2026-05-31 11:31:36 +0500

    Added SquareNumber program

---
+++ 01_Basics/Square Number.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num, square;
+	cout<<"..Printing Square of Numbers.."<<endl<<endl;
+	cin>>num;
+	square= num*num;
+	cout<<"Square is: "<<square;
+	return 0;
+}
