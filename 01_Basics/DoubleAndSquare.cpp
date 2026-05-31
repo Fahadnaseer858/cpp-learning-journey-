2f84d75492e3b420a8dd8768cec989c1a0926360
Fahad Naseer <fahadnaseer858@gmail.com>
2026-05-31 11:33:03 +0500

    Added DoubleAndSquare program

---
+++ 01_Basics/DoubleAndSquare.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n, Square, Double;
+	cout<<"..Enter input & print a number double and square \n \n";
+	cin>>n;
+	Double=n*2;
+	cout<<"Double of a number is: "<<Double<<endl;
+	
+	Square=n*n;
+	cout<<"Square of a number is: "<<Square<<endl;
+	
+	return 0;
+}
