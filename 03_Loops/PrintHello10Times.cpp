54524a6b73ce292880306a4b5df8a6958285f669
Fahad Naseer <fahadnaseer858@gmail.com>
2026-06-02 08:36:47 +0500

    Added PrintHello10Times program

---
+++ 03_Loops/PrintHello10Times.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"..Printing Hello 10 times through loops.."<<endl<<endl;
+	
+	for(a=1; a<=10; a++)
+	{
+		cout<<"Hello"<<endl;
+	}
+	return 0;
+}
