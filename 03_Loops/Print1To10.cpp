f0a6462be1ba174c0ebb9452622ea34d6c77c08c
Fahad Naseer <fahadnaseer858@gmail.com>
2026-06-02 08:38:40 +0500

    Added Print1To10 program

---
+++ 03_Loops/Print1To10.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	cout<<"..Printing Numbers from 1 to 10 through loops.."<<endl<<endl;
+	
+	for(i=1; i<=10; i++)
+	{
+	     cout<<i<<endl;
+	}
+	return 0;
+}
