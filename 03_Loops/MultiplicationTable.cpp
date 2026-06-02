94c2864010994652a704f06e18cbf1fc5da37def
Fahad Naseer <fahadnaseer858@gmail.com>
2026-06-02 08:40:15 +0500

    Added MultiplicationTable program

---
+++ 03_Loops/MultiplicationTable.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"...print table..."<<endl<<endl;
+	cin>>a;
+	
+	for(int b=1; b<=10; b++)
+	{
+
+		cout<<a<<" * "<<b<<" = "<<a*b<<endl;
+	}
+	return 0;
+}
